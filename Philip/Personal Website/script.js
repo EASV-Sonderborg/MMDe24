49a71dc66372e3cb8238fb7667b9b9c16be5bf26
Philip1911 <philip-brinck@hotmail.dk>
2024-09-05 22:05:41 +0200

    Website more wow
    woooooooooow

--- Philip/Personal Website/script.js
+++ Philip/Personal Website/script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {
     var Box = document.getElementById("Box");
     var originalHeight = 80; // Original height of .Box
-    var shrinkHeight = 50; // Height when scrolled
+    var shrinkHeight = 30; // Height when scrolled
 
     if (window.pageYOffset > 0) {
         Box.style.height = shrinkHeight + "px"; // Shrink height when scrolled
@@ -9,3 +9,5 @@ window.onscroll = function() {
         Box.style.height = originalHeight + "px"; // Reset height when at the top
     }
 };
+
+
