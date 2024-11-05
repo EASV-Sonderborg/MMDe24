ced2ba9ad9eb3e67510bc511f17e1f7c7c1c6391
Philip1911 <philip-brinck@hotmail.dk>
2024-11-05 23:17:43 +0100

    senCity

--- Philip/Sentinels/script.js
+++ Philip/Sentinels/script.js
@@ -1,25 +1,54 @@
 let productPage = 0;
 const   products = document.querySelectorAll(".product");
 const   nextProduct = 5000;
+let autoSlide;
 
-function showNextProduct() {
+function changeProduct(direction = 1) {
     const currentProduct = products[productPage];
 
     currentProduct.classList.add("fadeOut");
 
+    if (direction === 1) {
+        currentProduct.classList.add("slideOutLeft");
+    }
+    else    {
+        currentProduct.classList.add("slideOutRight");
+    }
     setTimeout(() => {
 
-        currentProduct.classList.remove("active");
-        currentProduct.classList.remove("fadeOut");
+        currentProduct.classList.remove("active", "fadeOut", "slideOutRight", "slideOutLeft");
 
-        productPage = (productPage + 1) % products.length;
+        productPage = (productPage + direction + products.length) % products.length;
 
-        products[productPage].classList.add("active");
+        const newProduct = products[productPage];
 
-        products[productPage].classList.remove("fadeOut");
-}, 1000);
+        newProduct.classList.remove("slideInLeft", "slideInRight");
+        newProduct.classList.add("active");
+
+
+        if  (direction === 1)   {
+            newProduct.classList.add("slideInRight");
+        }
+        else    {
+            newProduct.classList.add("slideInLeft");
+        }
+
+    }, 1000);
+}
+
+document.getElementById("nextButton").onclick = () => {
+    resetAutoSlide();
+    changeProduct(1);
+};
+document.getElementById("prevButton").onclick = () => {
+    resetAutoSlide();
+    changeProduct(-1);
+};
+
+function resetAutoSlide(){
+    clearInterval(autoSlide);
+    autoSlide = setInterval(() => changeProduct(1), nextProduct);
 }
 
 products[productPage].classList.add("active");
-
-setInterval(showNextProduct, nextProduct); 
+resetAutoSlide();
