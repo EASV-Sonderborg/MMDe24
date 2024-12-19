761d2ed4603cf662961452302694f925acc62ece
Franco Casenave <fraped01@easv365.dk>
2024-12-19 09:10:43 +0100

    Multimediedesigner MP 1 - Nærboks Full Commit

---
+++ Franco/Multimedierdesigner MP 1 - Franco/script.js
@@ -0,0 +1,12 @@
+console.log();
+
+document.querySelector(".language-selection").addEventListener("click", () => {
+  document.querySelector(".dropdown-menu").classList.toggle("show");
+});
+
+document.querySelector(".language-selection").addEventListener("click", () => {
+  document.querySelector(".dropdown-menu").classList.toggle("show");
+});
+document.querySelector(".language-selection").addEventListener("click", () => {
+  document.querySelector(".dropdown-menu").classList.toggle("show");
+});
