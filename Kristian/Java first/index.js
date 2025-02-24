24ebead31655520e9f7ff107fb6fd51b9eb069e7
Kristian Frederichsen <kristian.frederichsen@gmail.com>
2025-02-24 13:30:29 +0100

    j

--- Kristian/Java first/index.js
+++ Kristian/Java first/index.js
@@ -6,6 +6,8 @@ let kokkenKnivPris = 200
 
 let kokkenKnivAntal = 1
 
+
+
 let kurvAntal = kogeBogAntal + kokkenKnivAntal
 
 let kurvPris = (kogeBogPris * kogeBogAntal) + (kokkenKnivPris * kokkenKnivAntal)
@@ -17,18 +19,18 @@ console.log("Produkter:")
 
 console.log("Koge bog", kogeBogPris, "Kr." )
 
-console.log("Antal:", kogeBogAntal)
+console.log("Antal:", kogeBogAntal, "stk.")
 
 console.log("")
 
 console.log("Koge bog", kokkenKnivPris, "Kr." )
 
-console.log("Antal:", kokkenKnivAntal)
+console.log("Antal:", kokkenKnivAntal, "stk.")
 
 console.log("------------------------------")
 console.log("------------------------------")
 
-console.log("Samlet antal af produkter:", kurvAntal)
+console.log("Samlet antal af produkter:", kurvAntal, "stk.")
 
 console.log(" ")
 
@@ -36,3 +38,74 @@ console.log("Samlet pris:", kurvPris, "Kr.")
 console.log("------------------------------")
 
 console.log("[Køb nu]")
+
+
+
+
+/* -------------------- */
+
+function increment() {
+    kogeBogAntal = kogeBogAntal + 1
+    kurvPris = kurvPris + 70
+
+    console.log("------------------------------")
+    console.log("------------------------------")
+    console.log("kogebog antal:", kogeBogAntal)
+    console.log("Ny samlet pris:", kurvPris)
+}
+
+function increment2() {
+    kokkenKnivAntal = kokkenKnivAntal + 1
+    kurvPris = kurvPris + 200
+
+    console.log("------------------------------")
+    console.log("------------------------------")
+    console.log("køkkenkniv antal:", kokkenKnivAntal)
+    console.log("Ny samlet pris:", kurvPris)
+}
+
+function increment3() {
+    kogeBogAntal = kogeBogAntal - 1
+    kurvPris = kurvPris - 70
+
+    console.log("------------------------------")
+    console.log("------------------------------")
+    console.log("kogebog antal:", kogeBogAntal)
+    console.log("Ny samlet pris:", kurvPris)
+}
+
+function increment4() {
+    kokkenKnivAntal = kokkenKnivAntal - 1
+    kurvPris = kurvPris - 200
+
+    console.log("------------------------------")
+    console.log("------------------------------")
+    console.log("køkkenkniv antal:", kokkenKnivAntal)
+    console.log("Ny samlet pris:", kurvPris)
+}
+
+
+
+function update() {
+    document.getElementById("nbr")
+    kogeBogAntal = kogeBogAntal + nbr
+    kurvPris = kurvPris + 70
+
+    console.log("------------------------------")
+    console.log("------------------------------")
+    console.log("kogebog antal:", kogeBogAntal)
+    console.log("Ny samlet pris:", kurvPris)
+}
+
+
+/* -------------------- */
+
+/* function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown() */
