9d1470a5c84c30e5e2b4297d964f7651990a57fd
St0rkDk <116794306+St0rkDk@users.noreply.github.com>
2025-03-03 13:00:43 +0100

    Java script tester

---
+++ Martin/JavaScriptsTest/index.js
@@ -0,0 +1,34 @@
+//Billederene af produkterne
+
+
+
+//Ændre billede farve til sort
+function Black(){
+    
+    console.log("Change the color on the picture");
+}
+
+//Ændre billede farve til hvid
+function colorWhite(){ 
+
+}
+
+//Ændre produkts billede til størrelse small
+function SizeSmall() { 
+
+}
+
+//Ændre produkts billede til størrelse medium
+function SizeMedium() { 
+
+}
+
+//Ændre produkts billede til størrelse large
+function SizeLarge() { 
+
+}
+
+//Ændre prisen når det er sort og størlse medium og large
+function Saleprice() { 
+
+}
