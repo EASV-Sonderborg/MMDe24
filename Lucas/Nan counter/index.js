41784f12a4c9e1fa194afaf2f6fd5e0f581fe816
Luke-Beltoft <beltoftlucas@gmail.com>
2025-03-10 10:30:03 +0100

    Save nan counter to local storage

--- Lucas/Nan counter/index.js
+++ Lucas/Nan counter/index.js
@@ -1,6 +1,27 @@
-let count = 0;
+// let count = 0;
+let count = localStorage.getItem('slurCount') ? parseInt(localStorage.getItem('slurCount')) : 0;
+let countNumberEl = document.getElementById('countNumber')
+countNumberEl.innerText = count
+let inputAmount = localStorage.getItem('slurCount')
+
+const counterEl = document.querySelector('.counter');
+
+// Gem innertext counter / vis på reload 
+function updateCounterDisplay() {
+    document.querySelector('.counter').innerText = count;
+
+}
 
 function incrementCounter() {
     count++;
     document.querySelector('.counter').textContent = count;
+    localStorage.setItem('slurCount', count)
+
+}
+
+function removeSlurs() {
+    localStorage.removeItem('slurCount')
+    count = 0
+    counterEl.innerText = count 
+
 }
