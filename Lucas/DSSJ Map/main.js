224ecdc047bddca5243ea28e71c6bf2f3a460d22
Luke-Beltoft <beltoftlucas@gmail.com>
2025-06-15 17:09:42 +0200

    yaglorp

--- Lucas/DSSJ Map/main.js
+++ Lucas/DSSJ Map/main.js
@@ -29,7 +29,95 @@ const locations = {
         image: 'https://gdkfiles.visitdenmark.com/files/462/300980_Fluepapiret_i_Snderborg.jpg',
         link: 'https://www.visitsonderjylland.dk/turist/information/fluepapiret-soenderborg-gdk611120'
     },
-    // ... (other locations remain unchanged)
+
+    'strand-2': { 
+        lat: 54.89774821313825, 
+        lng: 9.803388089622732, 
+        name: 'Den Sorte Badestrand', 
+        type: 'beach',
+        description: 'Stranden har en badebro og en rampe i vandkanten, der giver personer i kørestol adgang til vandet.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/292130_Handicap-bro-ved-den-sorte_badestrand-i-Snderborg.jpg',
+        link: 'https://www.visitsonderjylland.dk/turist/information/den-sorte-badestrand-soenderborg-gdk611388'
+    },
+
+    'strand-3': { 
+        lat: 54.90602245391619, 
+        lng: 9.78045356960696, 
+        name: 'Dybbøl Strand', 
+        type: 'beach',
+        description: 'God plads for vadefiskeri ud mod Vemmingbund.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/180338_FV-Dybbl-Strand3.jpg',
+        link: 'https://www.visitsonderjylland.dk/turist/information/dybboel-strand-gdk1095969'
+    },
+    
+        // hiking/cycling
+    'tur-1': { 
+        lat: 55.02870284962671, 
+        lng: 9.727711284712766, 
+        name: 'Naturpark Nordals', 
+        type: 'hiking',
+        description: 'Den 1928 hektar store Naturpark Nordals byder på en varieret natur, med fredskove, strandenge og gendannede søer.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/258483_Bunds-en-del-af-Naturpark-Nordals.jpg',
+        link: 'https://www.visitsonderjylland.dk/turist/information/naturpark-nordals-gdk1123059'
+    },
+    'tur-2': { 
+        lat: 54.897745939739316, 
+        lng: 9.755679632797293, 
+        name: 'Gendarmstien', 
+        type: 'hiking',
+        description: 'Europæisk Kvalitetsvandrevej med LQT Best of Europe certifikat. 84 kilometer, hvor du kan vandre langs vandet, marker og gennem skove.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/211516_Gendarmsti-skilt.jpg',
+        link: 'https://www.visitsonderjylland.dk/turist/information/gendarmstien-gdk611136'
+    },
+
+    'tur-2.5': { 
+        lat: 54.882070428038375, 
+        lng: 9.990324669268485, 
+        name: 'Gendarmstien', 
+        type: 'hiking',
+        description: 'Europæisk Kvalitetsvandrevej med LQT Best of Europe certifikat. 84 kilometer, hvor du kan vandre langs vandet, marker og gennem skove.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/211516_Gendarmsti-skilt.jpg',
+        link: 'https://www.visitsonderjylland.dk/turist/information/gendarmstien-gdk611136'
+    },
+
+    'tur-3': { 
+        lat: 55.01565242122567, 
+        lng: 9.94464439009769, 
+        name: 'Alsstien', 
+        type: 'hiking',
+        description: 'Vandre rute på 64 kilometer, langs kyst, marker og i gennem skove på den smukke ø, Als.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/244064_Alsstien-ved-Oldenor.jpg?width=987',
+        link: 'https://www.visitsonderjylland.dk/turist/information/alsstien-gdk1108243'
+    },
+
+        // shopping
+    'shop-1': { 
+        lat: 54.90947216568481, 
+        lng: 9.791671861636711, 
+        name: 'Borgen Shopping', 
+        type: 'shopping',
+        description: 'Velkommen til en verden af inspiration, muligheder og oplevelser i Sønderjyllands største shopping center, Borgen Shopping.',
+        image: 'https://www.visitsonderjylland.dk/sites/visitsonderjylland.com/files/styles/hero/public/2022-01/Borgen-1-credit-Borgen-Shopping.jpg?h=19fdb4df&itok=QXiFraOJ',
+        link: 'https://www.visitsonderjylland.dk/turist/oplevelser/borgen'
+    },
+    'shop-2': { 
+        lat: 54.92353266389178, 
+        lng: 9.80975068923191, 
+        name: 'Dansk Sønderborg', 
+        type: 'shopping',
+        description: 'Spar penge med Dansk Outlet - Vi klæder hele familien på, fra top til tå, til fornuftige priser.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/304209_Dansk-Outlet-Snderborg.jpg?width=987',
+        link: 'https://www.visitsonderjylland.dk/turist/information/dansk-outlet-soenderborg-gdk1139417'
+    },
+    'shop-3': { 
+        lat: 54.91021609857013, 
+        lng: 9.789911183362994, 
+        name: 'WestWind Sønderborg', 
+        type: 'shopping',
+        description: 'Butikken med fritidsbeklædning til din families aktive ferie på øen Als.',
+        image: 'https://www.voressønderborg.dk/media/egvem045/west1.png',
+        link: 'https://www.westwind.dk'
+    },
 
     // culture
     'kultur-1': { 
@@ -47,7 +135,76 @@ const locations = {
         image: 'https://images.unsplash.com/photo-1570047382835-d4f548f10dc9?w=300&h=200&fit=crop',
         link: 'https://www.visitsonderborg.dk/sydhavn'
     },
-    // ... (other culture locations)
+
+        'kultur-3': { 
+        lat: 54.90718392709678, 
+        lng: 9.754253050545636, 
+        name: 'Historiecenter Dybbøl Banke', 
+        type: 'culture',
+        description: 'Træd ind i de dramatiske dage under krigen i 1864 og bliv en del af dramaet, mens du får ny viden om et af Danmarkshistoriens vigtigste slag.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/242525_HistorieCenterDybblBanke-kanon.jpg?width=987',
+        link: 'https://www.visitsonderjylland.dk/turist/information/historiecenter-dybboel-banke-gdk1084214'
+    },
+
+        // active
+    'aktiv-1': { 
+        lat: 55.04239869343909, 
+        lng: 9.809816073596984, 
+        name: 'Universe Science Park', 
+        type: 'active',
+        description: 'Der venter dig 3 nye oplevelser i 2025! Oplev spænding og sjov med hele familien i Sønderjyllands største oplevelsespark.',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/295719_Bl-kube-2022.jpg?width=987',
+        link: 'https://www.visitsonderjylland.com/tourist/universe-science-park/universe'
+    },
+    'aktiv-2': { 
+        lat: 54.97640248028504, 
+        lng: 9.885789159323457, 
+        name: 'Als Aktivitetspark', 
+        type: 'active',
+        description: 'Hos os får du frisk luft, mens du banker familie og venner i Discgolf, Fodboldgolf eller Krolf. I kan også øve Jer i teamwork i vores Mega-Bordfodbold!',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/326542_Als-Aktivitetspark-fodboldgolf-den-skal-vinkles.jpg?width=987',
+        link: 'https://www.visitsonderjylland.com/tourist/information/als-aktivitetspark-gdk732940'
+    },
+    'aktiv-3': { 
+        lat: 54.92011629753493, 
+        lng: 9.823530598199932, 
+        name: 'Sønderborg Padel Center', 
+        type: 'active',
+        description: 'Frisk luft og god motion - book en af vores 2 udendørs baner til padel tennis. ',
+        image: 'https://gdkfiles.visitdenmark.com/files/462/294959_Snderborg-Padel-Center.jpg?width=987',
+        link: 'https://www.visitsonderjylland.dk/turist/information/soenderborg-padel-center-gdk1136176'
+    },
+
+    // Mad
+    'food-1' : {
+        lat: 54.907948163855714, 
+        lng: 9.788034231352045, 
+        type: 'food',
+        slug: '422' 
+    },
+
+    'food-2' : {
+        lat: 54.90934410065719, 
+        lng: 9.7897079905029, 
+        type: 'food',
+        slug: '435' 
+    }, 
+
+    'food-3' : {
+        lat: 54.90784371580407, 
+        lng: 9.789061055977498, 
+        type: 'food',
+        slug: '444' 
+    },
+
+    'food-4' : {
+        lat: 54.9267033217532,
+        lng: 9.781954663087248,
+        type: 'food',
+        slug: '456'
+    },
+
+
 };
 
 // markers icons and functions unchanged
@@ -56,7 +213,8 @@ const markerSvgs = {
     hiking: 'https://dssj.naveedn.dk/wp-content/uploads/2025/05/ShoePin.svg',
     shopping: 'https://dssj.naveedn.dk/wp-content/uploads/2025/05/ShoppingPin.svg',
     culture: 'https://dssj.naveedn.dk/wp-content/uploads/2025/05/CulturePin.svg',
-    active: 'https://dssj.naveedn.dk/wp-content/uploads/2025/05/ActivePin.svg'
+    active: 'https://dssj.naveedn.dk/wp-content/uploads/2025/05/ActivePin.svg',
+    food: 'https://dssj.naveedn.dk/wp-content/uploads/2025/05/FoodPin.svg'
 };
 
 function createCustomIcon(type) {
@@ -200,8 +358,8 @@ function initMap() {
             icon: createCustomIcon(location.type)
         }).addTo(map);
 
-        // Special fetch for kultur-1 with slug
-        if (locationId === 'kultur-1' && location.slug) {
+        // Fetch post data using slug from JSON
+        if (location.slug) {
             marker.on('click', () => {
                 fetchPostAndShowPopup(marker, location.slug);
             });
@@ -250,6 +408,39 @@ function toggleLocation(locationId) {
     }
 }
 
+async function loadFoodLocations() {
+    try {
+        const response = await fetch('http://lucasbeltoft.dk/wp-json/wp/v2/posts/');
+        const data = await response.json();
+
+        const madCategoryContent = document.getElementById('mad-content');
+
+        data.forEach((place, index) => {
+            const placeId = `mad-${index + 1}`;
+            const title = place.title.rendered;
+
+            const location = document.createElement('div');
+            location.className = 'location';
+
+            location.innerHTML = `
+                <input type="checkbox" id="${placeId}" class="location__checkbox" checked onchange="toggleLocation('${placeId}')">
+                <label for="${placeId}" class="location__label">${title}</label>
+            `;
+
+            madCategoryContent.appendChild(location);
+        });
+
+    } catch (error) {
+        console.error('Fejl ved indlæsning af madsteder:', error);
+    }
+}
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     initMap();
+    loadFoodLocations();
 });
+
+
+
