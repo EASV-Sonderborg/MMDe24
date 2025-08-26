008e41b2a333b05f7fe0f6cf9324834ef123bcdf
Philip1911 <philip-brinck@hotmail.dk>
2025-08-26 22:56:40 +0200

    Portfolio Hero Background  nav DONE

--- Philip/React Portfolio/Portfolio React/src/Hero.jsx
+++ Philip/React Portfolio/Portfolio React/src/Hero.jsx
@@ -1,12 +1,24 @@
+import logo__html from './assets/logo__html.png';
+import logo__css from './assets/logo__css.png';
+import logo__js from './assets/logo__js.png';
+import logo__react from './assets/logo__react.png';
+
 function Hero() {
   return (
-    <section className="hero text__display">
+    <section className="hero">
       <div className="hero__content">
-        <h1 className="hero__title text__title">PHILIP BRINCK</h1>
+        <h1 className="hero__title text__display">PHILIP BRINCK</h1>
         <p className="hero__subtitle">Front-end Web Developer</p>
       </div>
       <div className="hero__skillset">
-        
+        <img src={logo__html} alt="skill__logo" />
+        <img src={logo__css} alt="skill__logo" />
+        <img src={logo__js} alt="skill__logo" />
+        <img src={logo__react} alt="skill__logo" />
+      </div>
+      <div className='hero__contact'>
+        <p className='text__body'>Skal vi arbejde sammen? Skriv til mig her</p>
+        <a className='contact__link glass' href="mailto: philip-brinck@hotmail.dk">Philip-brinck@hotmail.dk</a>
       </div>
     </section>
   );
--- Philip/React Portfolio/Portfolio React/src/main.jsx
+++ Philip/React Portfolio/Portfolio React/src/main.jsx
@@ -4,17 +4,32 @@ import './index.css'
 import './navbar.css'
 import './card.css'
 import './hero.css'
+
 import NavBar from './NavBar.jsx'
 import Card from './Card.jsx'
 import Hero from './Hero.jsx'
+import DotGrid from './DotGrid.jsx';
+
 
 createRoot(document.getElementById('root')).render(
 
     <StrictMode>
+      <DotGrid
+  dotSize={6}          // mindre dots
+  gap={20}             // lidt mere luft mellem dem
+  baseColor="#111625"  // mørke dots
+  activeColor="#5660ff"// lyser op når man hover
+  proximity={200}
+  shockRadius={300}
+  shockStrength={2}
+  resistance={900}
+  returnDuration={2}
+/>
       <NavBar />
+      <div className="animation"></div>
+    
       <Hero/> 
       <Card title="Project 1" description="This is a description of project 1." imageUrl="https://via.placeholder.com/150" />
-
     </StrictMode>
 
 )
